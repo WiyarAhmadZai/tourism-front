b34f33f84517bd7deaae89e83ceaea72cbad1114
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-11 19:35:56 +0430

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -40,4 +40,39 @@ api.interceptors.response.use(
   }
 );
 
+// Auth functions
+export const login = (credentials) => api.post('/login', credentials);
+export const register = (userData) => api.post('/register', userData);
+export const logout = () => api.post('/logout');
+
+// Destination functions
+export const getDestinations = (params) => api.get('/destinations', { params });
+export const getDestination = (id) => api.get(`/destinations/${id}`);
+
+// Tour Package functions
+export const getPackages = (params) => api.get('/tour-packages', { params });
+export const getPackage = (id) => api.get(`/tour-packages/${id}`);
+
+// Blog functions
+export const getBlogPosts = (params) => api.get('/blog-posts', { params });
+export const getBlogPost = (id) => api.get(`/blog-posts/${id}`);
+
+// Booking functions
+export const getBookings = () => api.get('/bookings');
+export const createBooking = (bookingData) => api.post('/bookings', bookingData);
+export const cancelBooking = (id) => api.delete(`/bookings/${id}`);
+
+// Cart functions
+export const getCart = () => api.get('/cart');
+export const addToCart = (item) => api.post('/cart', item);
+export const removeFromCart = (id) => api.delete(`/cart/${id}`);
+export const clearCart = () => api.delete('/cart');
+
+// Contact functions
+export const sendMessage = (messageData) => api.post('/contact', messageData);
+
+// User functions
+export const getUserProfile = () => api.get('/user');
+export const updateUserProfile = (userData) => api.put('/user', userData);
+
 export default api;
