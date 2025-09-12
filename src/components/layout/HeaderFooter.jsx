677302f26246c32c541d9e65d8304be614b7e036
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-12 10:23:38 +0430

    updated

--- src/components/layout/HeaderFooter.jsx
+++ src/components/layout/HeaderFooter.jsx
@@ -121,8 +121,11 @@ const HeaderFooter = () => {
         )}
       </header>
 
+      {/* Spacer to account for fixed header */}
+      <div className="h-16"></div>
+
       {/* Footer */}
-      <footer className="bg-gray-100 mt-16">
+      <footer className="bg-gray-100 mt-auto">
         <div className="container mx-auto px-4 py-12">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {/* About Section */}
