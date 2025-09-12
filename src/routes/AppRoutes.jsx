102c4f3d70645d291359b791ae5ca7bcd9c0b65b
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-12 11:59:16 +0430

    updated

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,40 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from '../App';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Home from '../pages/Home';
-import Explore from '../pages/Explore';
-import Destinations from '../pages/Destinations';
-import DestinationDetail from '../pages/DestinationDetail';
-import Packages from '../pages/Packages';
-import PackageDetail from '../pages/PackageDetail';
-import Gallery from '../pages/Gallery';
-import Blog from '../pages/Blog';
-import BlogDetail from '../pages/BlogDetail';
-import About from '../pages/About';
-import Contact from '../pages/Contact';
-import Profile from '../pages/Profile';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/destinations" element={<Destinations />} />
-        <Route path="/destination/:id" element={<DestinationDetail />} />
-        <Route path="/packages" element={<Packages />} />
-        <Route path="/package/:id" element={<PackageDetail />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<BlogDetail />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
