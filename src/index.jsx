70ec8bcded4906998a602120abb73a2771d84d04
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-12 12:05:32 +0430

    updated and deleted some files and also added some files

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRoutes from './routes/AppRoutes';
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <AppRoutes />
-      </CartProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>,
 );
