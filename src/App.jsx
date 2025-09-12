53506de10c88ac53d14758efba3179c473498309
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-12 16:37:44 +0430

    added new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LanguageProvider } from './contexts/LanguageContext';
 import './App.css';
 
 // Import page components
 import Home from './pages/Home';
-import About from './pages/About';
-import Tours from './pages/Tours';
-import Hotels from './pages/Hotels';
-import Restaurants from './pages/Restaurants';
-import Contact from './pages/Contact';
 
 // Import CSS for the CityTours design
 import './assets/css/bootstrap.min.css';
@@ -18,12 +12,11 @@ import './assets/css/vendors.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Afghanistan Tourism</h1>
-        <p>This is a simple React application.</p>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
